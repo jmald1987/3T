f3aab82fabd30bc4f96b06e233171bd52cf94406
jmald1987 <77485004+jmald1987@users.noreply.github.com>
2023-02-21 12:02:09 -0500

    Fifth  commit - JS init function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,20 @@ console.log("Your JS is linked up. Be the person you needed when you were little
 
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
+let board;
+
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    };
+    //be sure to call the init function!
+    init();
+
+function render() {
+    }
