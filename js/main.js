bd3d635c1ea30d666811d146cbfe69a00d45a1fb
jmald1987 <77485004+jmald1987@users.noreply.github.com>
2023-02-21 12:43:14 -0500

    nineth commit - JS getwinner function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 console.log("Your JS is linked up. Be the person you needed when you were little.");
 
 /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
+let win;
+//win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
 
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector('h2');
 
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
 
 /*----- functions -----*/
+function getWinner() {
+    // just stub it up for now.
+    }
+
 function handleTurn(event) {
     let idx = squares.findIndex(function(square) {
         return square === event.target;
@@ -26,6 +43,7 @@ function handleTurn(event) {
     // turn = 'X' 
     // };
     // writing the ternary saved you from all that. 
+    win = getWinner();
     render();
     };
 
@@ -45,4 +63,14 @@ function render() {
     board.forEach(function(mark, index){
         squares[index].textContent = mark;
         });
+        messages.textContent = `It's ${turn}'s turn!`;
     }
+    
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+        });
+        messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+        return winner;
+    };
